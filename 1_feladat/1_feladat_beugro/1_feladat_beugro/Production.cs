0ed05e1aab71a767bdd50c8481d1c209ec84a579
Viktoria Bognar <apalosaa4@gmail.com>
2022-06-10 21:58:43 +0200

    Production model class added

---
+++ 1_feladat/1_feladat_beugro/1_feladat_beugro/Production.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_feladat_beugro
+{
+    internal class Production
+    {
+        private int pcb_id;
+        public int Pcb_id
+        {
+            get { return pcb_id; }
+            set { pcb_id = value; }
+        }
+
+        private int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        private DateTime startDate;
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        private DateTime endDate;
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+    }
+}
